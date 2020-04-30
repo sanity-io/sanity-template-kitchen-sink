e7bb1dfccab782f209a734d6e0c5ff923aa8b74e
Rune Botten <rbotten@gmail.com>
2020-04-30 22:53:28 +0200

    no slug is frontpage

--- template/studio/resolvePreviewUrl.js
+++ template/studio/resolvePreviewUrl.js
@@ -1,9 +1,12 @@
 const env = process.env.NODE_ENV || 'development'
 
-export default function resolvePreviewUrl (document) {
+export default function resolvePreviewUrl(document) {
   const baseUrl = env === 'development' ? 'http://localhost:8000' : '<#<deployments.web.url>#>'
   switch (document._type) {
     case 'route':
+      if (!document.slug || !document.slug.current) {
+        return baseUrl
+      }
       return `${baseUrl}/${document.slug.current}`
     case 'post':
       return `${baseUrl}/blog/${document.slug.current}`
