37e1b86117eac6f3f805bfd58b0b9572a67f747e
Knut Melvær <knut.melvaer@gmail.com>
2020-05-06 09:45:47 +0200

    Add async slugify example

--- template/studio/schemas/documents/route.js
+++ template/studio/schemas/documents/route.js
@@ -1,4 +1,13 @@
+import client from 'part:@sanity/base/client'
 import { MdLink } from 'react-icons/lib/md'
+
+function myAsyncSlugifier(input) {
+  const query = '*[_id == $id][0]'
+  const params = {id: input._ref}
+  return client.fetch(query, params).then(doc => {
+    return doc.title.toLowerCase().replace(/\s+/g, '-').slice(0, 200)
+  })
+}
 
 export default {
   name: 'route',
@@ -37,6 +46,11 @@ export default {
           }
           return true
         }),
+      options: {
+        source: 'page',
+        // Read more: https://www.sanity.io/docs/slug-type
+        slugify: myAsyncSlugifier
+      }
     },
     {
       title: 'Use site title?',
