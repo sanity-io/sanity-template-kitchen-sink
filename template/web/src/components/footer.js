3fe119fdce97250ebffccef86b593b63c183c591
Rune Botten <rbotten@gmail.com>
2020-05-06 13:39:42 +0200

    Link to Page theme, Sanity docs and sanity slack community in footer

--- template/web/src/components/footer.js
+++ template/web/src/components/footer.js
@@ -37,22 +37,27 @@ const Footer = ({ siteTitle }) => (
           <p className="uppercase text-gray-500 md:mb-6">Links</p>
           <ul className="list-reset mb-6">
             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
-                FAQ
-              </span>
+              <a href="https://www.tailwindtoolbox.com/templates/landing-page">
+                <span className="hover:underline text-gray-800 hover:text-orange-500">
+                  Page theme
+                </span>
+              </a>
             </li>
             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
               <a
                 href="https://sanity.io/docs"
                 className="no-underline hover:underline text-gray-800 hover:text-orange-500"
               >
-                Help
+                Sanity Help
               </a>
             </li>
             <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-              <span className="no-underline hover:underline text-gray-800 hover:text-orange-500">
-                Support
-              </span>
+              <a
+                href="https://slack.sanity.io"
+                className="no-underline hover:underline text-gray-800 hover:text-orange-500"
+              >
+                Slack Community
+              </a>
             </li>
           </ul>
         </div>
